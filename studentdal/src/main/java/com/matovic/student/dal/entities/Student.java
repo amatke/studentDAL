f2fe9df5d38956a63b8db240a9c98ae76100ded4
Aleksa <lexamatovic@gmail.com>
2019-11-10 12:15:04 +0100

    Student DAL class

---
+++ studentdal/src/main/java/com/matovic/student/dal/entities/Student.java
@@ -0,0 +1,60 @@
+package com.matovic.student.dal.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="studenttab")
+public class Student {
+
+	@Id
+	private Long id;
+	@Column(name="sname")
+	private String name;
+	private String course;
+	private Double fee;
+	
+	public Student() {}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public Double getFee() {
+		return fee;
+	}
+	public void setFee(Double fee) {
+		this.fee = fee;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Student [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", course=");
+		builder.append(course);
+		builder.append(", fee=");
+		builder.append(fee);
+		builder.append("]");
+		return builder.toString();
+	}
+}
